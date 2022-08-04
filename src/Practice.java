b97d840d5158963f3f5ee5106d231cceec1a18ed
jstporty <zghkrsla@naver.com>
2022-08-04 18:08:04 +0900

    java0804

--- src/Practice.java
+++ src/Practice.java
@@ -1,6 +1,6 @@
-
-public class Practice {
-	public static void main(String[] args) {
+//
+//public class Practice {
+//	public static void main(String[] args) {
 //	for(int i =1;i<=7;i++) {
 //		System.out.println(fibo(i)+"\t");
 //	}
@@ -11,4 +11,14 @@ public class Practice {
 //	}
 //}
 
-//	j
+//		int before = 1;
+//		int after = 1;
+//		int next=0;
+//		for (int i = 0; i < 7; i++) {
+//			System.out.print(before + "\t");
+//			next = before + after;
+//			before= after;
+//			after=next;		
+//	}
+//}
+//}
